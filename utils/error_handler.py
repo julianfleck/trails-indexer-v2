cc4757b442d932c7a5c2c50df0280a5796e731d4
Julian Fleck <mail@julianfleck.net>
2023-10-27 01:57:57 -0400

    added timestamps, status tracker

--- utils/error_handler.py
+++ utils/error_handler.py
@@ -1,9 +1,17 @@
 import sys
 from rich.console import Console
 from rich import inspect
+from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn, TimeElapsedColumn
+from rich.status import Status
 from rich.traceback import Traceback
+from rich.live import Live
 from neo4j.exceptions import ServiceUnavailable
 from utils.config_loader import ConfigLoader
+
+class Timestamp:
+    def __init__(self):
+        from datetime import datetime
+        self.now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 class ErrorHandler:
     def __init__(self):
@@ -17,6 +25,10 @@ class ErrorHandler:
             Exception: self.exception
             # ... other error types ...
         }
+        self.timestamp = "[grey30][" + Timestamp().now + f"][/grey30]"
+        self.timestamp_success = "[green][" + Timestamp().now + f"] •[/green]"
+        self.timestamp_warning = "[orange][" + Timestamp().now + f"] •[/orange]"
+        self.timestamp_error = "[red][" + Timestamp().now + f"] •[/red]"
 
     def handle_error(self, error):
         error_type = type(error)
@@ -26,6 +38,31 @@ class ErrorHandler:
     def inspect_object(self, obj):
         self.console.print(f"[bold blue]Inspecting object[/bold blue]")
         self.console.print(f"{inspect(obj)}")
+
+    def track_status(self, func, *args, description="Processing..."):
+        """
+        Display a progress bar with the given description while executing the provided function.
+        Returns the result of the executed function.
+        """
+        result = None
+        self.console.print(self.timestamp + f" {description}")
+        # with Status(f"Running – {description}", spinner="point") as status:
+        with Progress(
+            SpinnerColumn(spinner_name="point", style="bold blue"),
+            TextColumn("        "),
+            TimeElapsedColumn(),
+            # *Progress.get_default_columns(),
+            TextColumn(" [progress.description]{task.description}"),
+            transient=True
+        ) as progress:
+            task = progress.add_task(f"Running...", total=None)
+            try:
+                result = func(*args)
+            except Exception as e:
+                self.console.print(self.timestamp_error + f"Error: {e}")
+                self.exception(sys.exc_info())
+                raise e
+        return result
 
     def service_unavailable(self, error):
         self.console.print(f"[bold red]Service Unavailable:[/bold red] {error}")
@@ -40,11 +77,9 @@ class ErrorHandler:
         self.console.print(f"[bold red]Value Error:[/bold red] {error}")
 
     def generic_error(self, message, exception=None):
-        self.console.print(f"[bold red]Error:[/bold red] {message}")
+        self.console.print(self.timestamp_error + f" {message}")
         if exception:
-            print(f"Error: {message}. Exception: {exception}")
-        else:
-            print(f"Error: {message}")
+            self.console.inspect(exception)
 
     def exception(self, exc_info):
         self.console.print(Traceback.from_exception(*exc_info))
@@ -52,15 +87,15 @@ class ErrorHandler:
     def success(self, message):
         # only print when GENERAL_CONFIG / DEBUG is set to True
         if self.config.get_general_config()['DEBUG']:
-            self.console.print(f"[bold green]{message}[/bold green]")
+            self.console.print(self.timestamp_success + f" {message}")
 
     def debug_info(self, message):
         # only print when GENERAL_CONFIG / DEBUG is set to True
         if self.config.get_general_config()['DEBUG']:
-            self.console.print(f"{message}")
+            self.console.print(self.timestamp + f" {message}")
 
     def warning(self, message):
-        self.console.print(f"[bold yellow]{message}[/bold yellow]")
+            self.console.print(self.timestamp_warning + f" {message}")
 
 
 if __name__ == "__main__":
